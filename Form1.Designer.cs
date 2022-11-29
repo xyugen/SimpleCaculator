0ae341b54dadea3007e68b643f0b4a8afaa535db
Alex Arias <alex.arias.jorym@gmail.com>
2022-11-29 21:22:59 +0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,332 @@
+﻿namespace SimpleCaculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.txtBox_res = new System.Windows.Forms.TextBox();
+            this.btn_1 = new System.Windows.Forms.Button();
+            this.btn_2 = new System.Windows.Forms.Button();
+            this.btn_3 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.btn_4 = new System.Windows.Forms.Button();
+            this.btn_5 = new System.Windows.Forms.Button();
+            this.btn_6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.btn_7 = new System.Windows.Forms.Button();
+            this.btn_8 = new System.Windows.Forms.Button();
+            this.btn_9 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.btn_point = new System.Windows.Forms.Button();
+            this.btn_0 = new System.Windows.Forms.Button();
+            this.btn_equal = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.btn_reset = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.btn_reset);
+            this.flowLayoutPanel1.Controls.Add(this.btn_1);
+            this.flowLayoutPanel1.Controls.Add(this.btn_2);
+            this.flowLayoutPanel1.Controls.Add(this.btn_3);
+            this.flowLayoutPanel1.Controls.Add(this.button3);
+            this.flowLayoutPanel1.Controls.Add(this.btn_4);
+            this.flowLayoutPanel1.Controls.Add(this.btn_5);
+            this.flowLayoutPanel1.Controls.Add(this.btn_6);
+            this.flowLayoutPanel1.Controls.Add(this.button7);
+            this.flowLayoutPanel1.Controls.Add(this.btn_7);
+            this.flowLayoutPanel1.Controls.Add(this.btn_8);
+            this.flowLayoutPanel1.Controls.Add(this.btn_9);
+            this.flowLayoutPanel1.Controls.Add(this.button11);
+            this.flowLayoutPanel1.Controls.Add(this.btn_point);
+            this.flowLayoutPanel1.Controls.Add(this.btn_0);
+            this.flowLayoutPanel1.Controls.Add(this.btn_equal);
+            this.flowLayoutPanel1.Controls.Add(this.button15);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 93);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(340, 381);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // txtBox_res
+            // 
+            this.txtBox_res.Font = new System.Drawing.Font("Segoe UI", 22F);
+            this.txtBox_res.Location = new System.Drawing.Point(12, 12);
+            this.txtBox_res.Multiline = true;
+            this.txtBox_res.Name = "txtBox_res";
+            this.txtBox_res.Size = new System.Drawing.Size(340, 75);
+            this.txtBox_res.TabIndex = 1;
+            this.txtBox_res.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.txtBox_res.TextChanged += new System.EventHandler(this.txtBox_res_TextChanged);
+            // 
+            // btn_1
+            // 
+            this.btn_1.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.btn_1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_1.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btn_1.Location = new System.Drawing.Point(3, 43);
+            this.btn_1.Name = "btn_1";
+            this.btn_1.Size = new System.Drawing.Size(79, 79);
+            this.btn_1.TabIndex = 0;
+            this.btn_1.Text = "1";
+            this.btn_1.UseVisualStyleBackColor = false;
+            this.btn_1.Click += new System.EventHandler(this.btn_1_Click);
+            // 
+            // btn_2
+            // 
+            this.btn_2.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.btn_2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_2.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btn_2.Location = new System.Drawing.Point(88, 43);
+            this.btn_2.Name = "btn_2";
+            this.btn_2.Size = new System.Drawing.Size(79, 79);
+            this.btn_2.TabIndex = 0;
+            this.btn_2.Text = "2";
+            this.btn_2.UseVisualStyleBackColor = false;
+            this.btn_2.Click += new System.EventHandler(this.btn_2_Click);
+            // 
+            // btn_3
+            // 
+            this.btn_3.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.btn_3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_3.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btn_3.Location = new System.Drawing.Point(173, 43);
+            this.btn_3.Name = "btn_3";
+            this.btn_3.Size = new System.Drawing.Size(79, 79);
+            this.btn_3.TabIndex = 0;
+            this.btn_3.Text = "3";
+            this.btn_3.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.PaleGoldenrod;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button3.Location = new System.Drawing.Point(258, 43);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(79, 79);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "+";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // btn_4
+            // 
+            this.btn_4.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.btn_4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_4.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btn_4.Location = new System.Drawing.Point(3, 128);
+            this.btn_4.Name = "btn_4";
+            this.btn_4.Size = new System.Drawing.Size(79, 79);
+            this.btn_4.TabIndex = 0;
+            this.btn_4.Text = "4";
+            this.btn_4.UseVisualStyleBackColor = false;
+            // 
+            // btn_5
+            // 
+            this.btn_5.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.btn_5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_5.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btn_5.Location = new System.Drawing.Point(88, 128);
+            this.btn_5.Name = "btn_5";
+            this.btn_5.Size = new System.Drawing.Size(79, 79);
+            this.btn_5.TabIndex = 0;
+            this.btn_5.Text = "5";
+            this.btn_5.UseVisualStyleBackColor = false;
+            // 
+            // btn_6
+            // 
+            this.btn_6.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.btn_6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_6.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btn_6.Location = new System.Drawing.Point(173, 128);
+            this.btn_6.Name = "btn_6";
+            this.btn_6.Size = new System.Drawing.Size(79, 79);
+            this.btn_6.TabIndex = 0;
+            this.btn_6.Text = "6";
+            this.btn_6.UseVisualStyleBackColor = false;
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.PaleGoldenrod;
+            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button7.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button7.Location = new System.Drawing.Point(258, 128);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(79, 79);
+            this.button7.TabIndex = 0;
+            this.button7.Text = "-";
+            this.button7.UseVisualStyleBackColor = false;
+            // 
+            // btn_7
+            // 
+            this.btn_7.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.btn_7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_7.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btn_7.Location = new System.Drawing.Point(3, 213);
+            this.btn_7.Name = "btn_7";
+            this.btn_7.Size = new System.Drawing.Size(79, 79);
+            this.btn_7.TabIndex = 0;
+            this.btn_7.Text = "7";
+            this.btn_7.UseVisualStyleBackColor = false;
+            // 
+            // btn_8
+            // 
+            this.btn_8.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.btn_8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_8.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btn_8.Location = new System.Drawing.Point(88, 213);
+            this.btn_8.Name = "btn_8";
+            this.btn_8.Size = new System.Drawing.Size(79, 79);
+            this.btn_8.TabIndex = 0;
+            this.btn_8.Text = "8";
+            this.btn_8.UseVisualStyleBackColor = false;
+            // 
+            // btn_9
+            // 
+            this.btn_9.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.btn_9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_9.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btn_9.Location = new System.Drawing.Point(173, 213);
+            this.btn_9.Name = "btn_9";
+            this.btn_9.Size = new System.Drawing.Size(79, 79);
+            this.btn_9.TabIndex = 0;
+            this.btn_9.Text = "9";
+            this.btn_9.UseVisualStyleBackColor = false;
+            // 
+            // button11
+            // 
+            this.button11.BackColor = System.Drawing.Color.PaleGoldenrod;
+            this.button11.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button11.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button11.Location = new System.Drawing.Point(258, 213);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(79, 79);
+            this.button11.TabIndex = 0;
+            this.button11.Text = "*";
+            this.button11.UseVisualStyleBackColor = false;
+            // 
+            // btn_point
+            // 
+            this.btn_point.BackColor = System.Drawing.Color.PaleGreen;
+            this.btn_point.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_point.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btn_point.Location = new System.Drawing.Point(3, 298);
+            this.btn_point.Name = "btn_point";
+            this.btn_point.Size = new System.Drawing.Size(79, 79);
+            this.btn_point.TabIndex = 0;
+            this.btn_point.Text = ".";
+            this.btn_point.UseVisualStyleBackColor = false;
+            // 
+            // btn_0
+            // 
+            this.btn_0.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.btn_0.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_0.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btn_0.Location = new System.Drawing.Point(88, 298);
+            this.btn_0.Name = "btn_0";
+            this.btn_0.Size = new System.Drawing.Size(79, 79);
+            this.btn_0.TabIndex = 0;
+            this.btn_0.Text = "0";
+            this.btn_0.UseVisualStyleBackColor = false;
+            // 
+            // btn_equal
+            // 
+            this.btn_equal.BackColor = System.Drawing.Color.PaleGreen;
+            this.btn_equal.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_equal.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btn_equal.Location = new System.Drawing.Point(173, 298);
+            this.btn_equal.Name = "btn_equal";
+            this.btn_equal.Size = new System.Drawing.Size(79, 79);
+            this.btn_equal.TabIndex = 0;
+            this.btn_equal.Text = "=";
+            this.btn_equal.UseVisualStyleBackColor = false;
+            // 
+            // button15
+            // 
+            this.button15.BackColor = System.Drawing.Color.PaleGoldenrod;
+            this.button15.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button15.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button15.Location = new System.Drawing.Point(258, 298);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(79, 79);
+            this.button15.TabIndex = 0;
+            this.button15.Text = "/";
+            this.button15.UseVisualStyleBackColor = false;
+            // 
+            // btn_reset
+            // 
+            this.btn_reset.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btn_reset.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_reset.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_reset.Location = new System.Drawing.Point(3, 3);
+            this.btn_reset.Name = "btn_reset";
+            this.btn_reset.Size = new System.Drawing.Size(334, 34);
+            this.btn_reset.TabIndex = 1;
+            this.btn_reset.Text = "Reset";
+            this.btn_reset.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(364, 486);
+            this.Controls.Add(this.txtBox_res);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.TextBox txtBox_res;
+        private System.Windows.Forms.Button btn_reset;
+        private System.Windows.Forms.Button btn_1;
+        private System.Windows.Forms.Button btn_2;
+        private System.Windows.Forms.Button btn_3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btn_4;
+        private System.Windows.Forms.Button btn_5;
+        private System.Windows.Forms.Button btn_6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button btn_7;
+        private System.Windows.Forms.Button btn_8;
+        private System.Windows.Forms.Button btn_9;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button btn_point;
+        private System.Windows.Forms.Button btn_0;
+        private System.Windows.Forms.Button btn_equal;
+        private System.Windows.Forms.Button button15;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleCaculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtBox_res_TextChanged(object sender, EventArgs e)
+        {
+            if (Char.IsDigit(txtBox_res.Text.All()))
+            {
+
+            }
+        }
+
+        private void btn_1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
